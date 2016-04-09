b3ab00956120b56bb39d7bb0380c2edadd3e9cb5
Smasty <martin@smasty.net>
2016-04-10 01:37:14 +0200

    Check if package dependencies have new version. Warning - Dashboards are not generated at this time.

--- lunaci/Manager.lua
+++ lunaci/Manager.lua
@@ -5,7 +5,11 @@ local utils = require "lunaci.utils"
 local config = require "lunaci.config"
 local Worker = require "lunaci.Worker"
 
+local const = require "rocksolver.constraints"
+
 local pl = require "pl.import_into"()
+
+local plsort = pl.tablex.sort
 
 
 local Manager = {}
@@ -76,21 +80,44 @@ end
 
 
 function Manager:get_packages()
-    return pl.tablex.sort(self.manifest.packages)
+    return plsort(self.manifest.packages)
 end
 
 
-function Manager:package_changed(name)
+function Manager:has_new_dependencies(name, ver)
+    local deps = self.manifest.packages[name][ver].dependencies or {}
+    for _, dep in pairs(deps) do
+        local dep_name, dep_const = const.split(dep)
+        local new_dep_vers = self:get_changed_versions(dep_name) or {}
+        for new_ver in pairs(new_dep_vers) do
+            if const.constraint_satisified(new_ver, dep_const) then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+
+function Manager:get_changed_versions(name)
     if not self.last_manifest then
         return self.manifest.packages[name]
     end
-    local current = self.manifest.packages[name]
-    local last = self.last_manifest.packages[name]
+    local current = self.manifest.packages[name] or {}
+    local last = self.last_manifest.packages[name] or {}
 
-    if pl.tablex.deepcompare(current, last) then
-        return nil
+    -- New versions of package
+    local new_versions = pl.tablex.difference(current, last) or {}
+
+    -- Check for new versions of relevant dependencies
+    for ver, spec in plsort(current, utils.sortVersions) do
+        if self:has_new_dependencies(name, ver) and not new_versions[ver] then
+            new_versions[ver] = spec
+        end
     end
-    return pl.tablex.difference(current, last)
+
+
+    return new_versions
 end
 
 
@@ -104,29 +131,28 @@ function Manager:process_packages()
 
     for name in self:get_packages() do
         log:info("Processing package '%s'", name)
-        local versions = self:package_changed(name)
-        pl.pretty.dump(versions)
-        if not versions then
-            log:debug("No changes in package '%s'", name)
-        else
-            local worker = Worker(name, versions, self.manifest)
-            log:debug("New versions for package '%s':", name)
-            for v in pairs(versions) do
-                log:debug("- %s", v)
-            end
+        local new_versions = self:get_changed_versions(name)
+        --[[ For debug purposes
+        for v in plsort(new_versions or {}, utils.sortVersions) do
+            log:debug("- %s", v)
+        end
+        --]]
+        if new_versions then
+
+            local worker = Worker(name, new_versions, self.manifest)
             worker:run(self.targets, self.tasks)
 
             -- Generate package reports
             self.generator:add_report(name, worker:get_report())
-            for version in pl.tablex.sort(versions, utils.sortVersions) do
+            for version in plsort(new_versions, utils.sortVersions) do
                 self.generator:generate_package_version(name, version)
             end
-            self.generator:generate_package(name)
+            --self.generator:generate_package(name)
         end
     end
 
     -- Generate dashboard report
-    self.generator:generate_dashboard()
+    --self.generator:generate_dashboard()
 
 
     -- TODO move elsewhere
